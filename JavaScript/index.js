7f973026e4a751d162b350a667bba699c740d98a
Monroe-spec <monroe.prgm@gmail.com>
2025-02-04 09:06:40 -0700

    calculator Pt 2

---
+++ JavaScript/index.js
@@ -0,0 +1,30 @@
+//function additionFunction(parameter1, parameter2) {
+//              code here
+//    return parameter1 + parameter2;
+//}
+
+function add() {
+    const num = getValue();
+    value += num;
+    setValue();
+}
+
+function sub() {
+    const num = getValue();
+    value -= num;
+    setValue();
+}
+
+function mult() {
+    const num = getValue();
+    value *= num;
+    setValue();
+}
+
+function div() {
+    const num = getValue();
+    value /= num;
+    setValue();
+}
+
+setValue();
