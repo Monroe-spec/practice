3294b31dd311314e1000101034ec42a7c752ba5e
Monroe-spec <monroe.prgm@gmail.com>
2025-02-12 08:42:58 -0700

    object lesson

---
+++ JavaScript/object oriented/obj.js
@@ -0,0 +1,13 @@
+// objects are a const. that has variables put in in a specific catagory, for example, you can put the make, model, color, and year of a car in an object.
+
+//ex
+
+// const car = {
+//     make: "Toyota",
+//     model: "Corolla",
+//     color: "Blue",
+//     year: 2004
+// }
+
+// use "this." to refer to an object as a whole, such as "this.make" or "this.model".
+// That way when you type in the object name, it will refer to the object as a whole, and not just the variable.
